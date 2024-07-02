e5378707ffacc4220a4d2c6b690c038af908c85c
adheena_seju <adheenaseju@gmail.com>
2024-07-02 05:16:12 +0400

    added the subjects name and code to mainpagebody

--- teach-assist/src/app/mainpage/components/mainpagebody/mainpagebody.component.ts
+++ teach-assist/src/app/mainpage/components/mainpagebody/mainpagebody.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-mainpagebody',
   standalone: true,
-  imports: [],
   templateUrl: './mainpagebody.component.html',
-  styleUrl: './mainpagebody.component.css'
+  styleUrl: './mainpagebody.component.css',
+  imports: [CommonModule]
 })
 export class MainpagebodyComponent {
-
+  subjects = [
+    { code: 'Comp30023', name: 'Computer Systems' },
+    { code: 'Comp10023', name: 'Introduction to the New World' }
+  ];
 }
