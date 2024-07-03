ec604f8871c9a75cdd665c07bd334c444a80ef12
adheena_seju <adheenaseju@gmail.com>
2024-07-03 04:55:59 +0400

    Add the options in teacherpage and student page and  link to upload page and style updates for boxes

--- teach-assist/src/app/teachersubjectpage/teachersubjectpage.component.ts
+++ teach-assist/src/app/teachersubjectpage/teachersubjectpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-teachersubjectpage',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './teachersubjectpage.component.html',
   styleUrl: './teachersubjectpage.component.css'
 })
