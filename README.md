1121804697343466d9f6fb5914dbb8e2daa0084e
maheen562 <maheenkhan193@gmail.com>
2024-07-31 12:48:00 +1000

    added README

---
+++ README.md
@@ -0,0 +1,9 @@
+This project is developed in angular
+
+
+Pre-req: Install angular
+
+Run:
+
+cd teach-assist
+ng s
--- teach-assist/src/app/login/login.component.ts
+++ teach-assist/src/app/login/login.component.ts
@@ -25,24 +25,25 @@ export class LoginComponent {
     loginUser() {
       if (this.username && this.password) {
         // Implement your login logic here, for example, call an API to authenticate the user
+         this.router.navigate(['mainpage']);
 
         // Navigate to the main page after successful login
-          this.loginService.getUserDetails(this.username,this.password,this.type).subscribe(
-            (response: any) => {
-              if (response && response.status === true && response.token) {
-                // Login successful, navigate to main page
-                this.router.navigate(['mainpage']);
-              } else {
-                // Handle invalid login response (if needed)
-                this.errorMessage = 'Username or password incorrect.';
-              }
-            },
-            (error) => {
-              console.error('Error:', error);
-              // Handle HTTP error response
-              this.errorMessage = 'username or password incorrect';
-            }
-          );
+          // this.loginService.getUserDetails(this.username,this.password,this.type).subscribe(
+          //   (response: any) => {
+          //     if (response && response.status === true && response.token) {
+          //       // Login successful, navigate to main page
+          //       this.router.navigate(['mainpage']);
+          //     } else {
+          //       // Handle invalid login response (if needed)
+          //       this.errorMessage = 'Username or password incorrect.';
+          //     }
+          //   },
+          //   (error) => {
+          //     console.error('Error:', error);
+          //     // Handle HTTP error response
+          //     this.errorMessage = 'username or password incorrect';
+          //   }
+          // );
         
       } else {
         this.errorMessage = 'Please enter both username and password.';
